0e0d5b894c4fbb479388e69684e4629d47258c2c
arsahak <arsahak99@gmail.com>
2025-12-02 15:28:03 +0600

    first commit

---
+++ routes/controllers/website/categoryData.js
@@ -0,0 +1,12 @@
+import Category from "../../../database/models/Category.js";
+
+const category_data_site = async (req, res) => {
+  try {
+    const categories = await Category.find().sort({ createdAt: -1 });
+    return res.status(200).json({ data: categories });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+};
+
+export default category_data_site;
---
+++ server.js
@@ -0,0 +1,30 @@
+// Imports
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import http from "http";
+import connectDB from "./database/connection.js";
+import router from "./routes/routeNames.js";
+
+// Configurations
+dotenv.config();
+
+// App setup
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+connectDB();
+
+//test Routes
+app.get("/", (req, res) => {
+  return res.status(200).json({ success: "response from get api" });
+});
+
+// Server setup
+const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`server run at ${port}`);
+});
